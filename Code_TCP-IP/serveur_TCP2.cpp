200038684c39208004668acea948a82d68be03bc
enotelu <125290192+enotelu@users.noreply.github.com>
2023-06-25 14:27:55 +0200

    FINAL --> code commenté

--- Code_TCP-IP/serveur_TCP2.cpp
+++ Code_TCP-IP/serveur_TCP2.cpp
@@ -33,41 +33,62 @@ Avant de lancer le programme
 #define PIN_M2 22  // Broche 22 (GPIO 6)
 
 void STOP(void) {
+  // Arrête les moteurs en mettant les sorties de la vitesse (pwm) à 100 (valeur maximale)
   pwmWrite(PIN_E1, 100);
+  // Met la direction du moteur 1 en LOW (arrêt)
   digitalWrite(PIN_M1, LOW);
+  // Met la sortie de la vitesse du moteur 2 à 100
   pwmWrite(PIN_E2, 100);
+  // Met la direction du moteur 2 en LOW (arrêt)
   digitalWrite(PIN_M2, LOW);
 }
 
 void AVANT(int a, int b) {
+  // Définit la vitesse du moteur 1 avec la valeur a
   pwmWrite(PIN_E1, a);
+  // Met la direction du moteur 1 en LOW (mouvement vers l'avant)
   digitalWrite(PIN_M1, LOW);
+  // Définit la vitesse du moteur 2 avec la valeur b
   pwmWrite(PIN_E2, b);
+  // Met la direction du moteur 2 en HIGH (mouvement vers l'avant)
   digitalWrite(PIN_M2, HIGH);
 }
 
 void ARRIERE(int a, int b) {
+  // Définit la vitesse du moteur 1 avec la valeur a
   pwmWrite(PIN_E1, a);
+  // Met la direction du moteur 1 en HIGH (mouvement vers l'arrière)
   digitalWrite(PIN_M1, HIGH);
+  // Définit la vitesse du moteur 2 avec la valeur b
   pwmWrite(PIN_E2, b);
+  // Met la direction du moteur 2 en LOW (mouvement vers l'arrière)
   digitalWrite(PIN_M2, LOW);
 }
 
 void GAUCHE(int a, int b) {
+  // Définit la vitesse du moteur 1 avec la valeur a
   pwmWrite(PIN_E1, a);
+  // Met la direction du moteur 1 en HIGH (mouvement vers l'arrière)
   digitalWrite(PIN_M1, HIGH);
+  // Définit la vitesse du moteur 2 avec la valeur b
   pwmWrite(PIN_E2, b);
+  // Met la direction du moteur 2 en HIGH (mouvement vers l'avant)
   digitalWrite(PIN_M2, HIGH);
 }
 
 void DROITE(int a, int b) {
+  // Définit la vitesse du moteur 1 avec la valeur a
   pwmWrite(PIN_E1, a);
+  // Met la direction du moteur 1 en LOW (mouvement vers l'avant)
   digitalWrite(PIN_M1, LOW);
+  // Définit la vitesse du moteur 2 avec la valeur b
   pwmWrite(PIN_E2, b);
+  // Met la direction du moteur 2 en LOW (mouvement vers l'arrière)
   digitalWrite(PIN_M2, LOW);
 }
 
 void setup(void) {
+  // Initialise la bibliothèque WiringPi
   wiringPiSetup();
 
   // Configuration des broches de commande du moteur
@@ -76,20 +97,25 @@ void setup(void) {
   pinMode(PIN_M1, OUTPUT);
   pinMode(PIN_M2, OUTPUT);
 
+  // Configuration du mode de modulation de largeur d'impulsion (PWM)
   pwmSetMode(PWM_MODE_MS);
+  // Configuration de la plage de valeurs PWM (de 0 à 100)
+  // Le montage électronique suggère que 100=STOP, 0=Vmax
   pwmSetRange(100);
 
-  // Arrêt initial
+  // Arrêt initial des moteurs
   STOP();
 }
 
 int main() {
+    // Déclaration des variables
     int serverSocket, newSocket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
 
+    // Configuration et initialisation
     setup();
 
     // Création du socket serveur
@@ -103,6 +129,8 @@ int main() {
         perror("Échec de l'attachement du socket au port");
         exit(EXIT_FAILURE);
     }
+
+    // Configuration de l'adresse du socket
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
@@ -134,6 +162,7 @@ int main() {
         // Réception des données du client
         valread = read(newSocket, buffer, BUFFER_SIZE);
 
+        // Gestion des erreurs de lecture du socket
         if (valread == 0) {
             printf("Client déconnecté.\n");
             break;
@@ -148,8 +177,7 @@ int main() {
         char commande = buffer[0];
         printf("Commande convertie en char : %c\n", commande);
 
-        // Traiter les commandes reçues et converties
-
+        // Traitement des commandes reçues et converties
         switch (commande) {
             case 'z':
                 printf("AVANT\n");
@@ -178,11 +206,13 @@ int main() {
             default:
                 STOP();
                 break;
-            }
+        }
     }
 
+    // Fermeture des sockets
     close(newSocket);
     close(serverSocket);
 
     return 0;
 }
+
